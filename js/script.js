7e06df0050ed800f4334ad985cbc39bda03cc745
rafaelmoz <38104677+rafaelmoz@users.noreply.github.com>
2019-06-03 15:12:17 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+(function(){
+
+    'use strict';
+
+
+    var $projects = $('.projects');
+
+    $projects.isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
+    });
+
+    $('ul.filters > li').on('click', function(e){
+
+        e.preventDefault();
+
+        var filter = $(this).attr('data-filter');
+
+        $('ul.filters > li').removeClass('active');
+        $(this).addClass('active');
+
+        $projects.isotope({filter: filter});
+
+    });
+
+    $('.card').mouseenter(function(){
+
+        $(this).find('.card-overlay').css({'top': '-100%'});
+        $(this).find('.card-hover').css({'top':'0'});
+
+    }).mouseleave(function(){
+
+        $(this).find('.card-overlay').css({'top': '0'});
+        $(this).find('.card-hover').css({'top':'100%'});
+
+    });
+
+    var $page = $('.page');
+    
+    $('.menu_toggle').on('click', function(){
+        $page.toggleClass('shazam');
+    });
+
+    $('.content').on('click', function(){
+        $page.removeClass('shazam');
+    });
+
+    window.sr = ScrollReveal();
+    sr.reveal('.anim');
+
+    var $page = $('.page');
+
+    $('.menu_toggle').on('click', function(){
+        $page.toggleClass('shazam');
+    });
+
+    $('.content').on('click', function(){
+        $page.removeClass('shazam');
+    });
+
+})(jQuery);
